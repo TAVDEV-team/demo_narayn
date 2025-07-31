f51bc853bd27e3d5f51e241bbcbdb0136b593a8b
TrishaPal2003 <your-email@example.com>
2025-07-31 08:22:54 +0600

    commit

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,22 +1,22 @@
-import { api } from "./api";
+// import { api } from "./api";
 
-export const registerUser = (data) => api.post("users/register/", data);
+// export const registerUser = (data) => api.post("users/register/", data);
 
-export const loginUser = (data) => api.post("users/login/", data);
+// export const loginUser = (data) => api.post("users/login/", data);
 
-export const isAuthenticated = () => !!localStorage.getItem("Token");
+// export const isAuthenticated = () => !!localStorage.getItem("Token");
 
-export const getToken = () => localStorage.getItem("token");
+// export const getToken = () => localStorage.getItem("token");
 
-export const setAuthData = (token, userId) => {
-  localStorage.setItem("token", token);
-  localStorage.setItem("userId", userId);
-};
+// export const setAuthData = (token, userId) => {
+//   localStorage.setItem("token", token);
+//   localStorage.setItem("userId", userId);
+// };
 
-export const clearAuthData = () => {
-  localStorage.removeItem("token");
-  localStorage.removeItem("user");
-};
+// export const clearAuthData = () => {
+//   localStorage.removeItem("token");
+//   localStorage.removeItem("user");
+// };
 
 
 
