e67ee101c16ac802ba33be2d3f6998538cc1a269
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-08-26 06:26:36 +0600

    problem solved

--- src/pages/HeadMasterProfile.jsx
+++ src/pages/HeadMasterProfile.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-const [sidebarOpen, setSidebarOpen] = useState(false);
+// const [sidebarOpen, setSidebarOpen] = useState(false);
 
 import {
   MapPin,
